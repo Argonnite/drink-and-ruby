1ce297ab2bedd0c0fface341c1848bede624659c
Ron Galicia <dermalin3k@hotmail.com>
2015-07-07 19:54:08 -0700

    Gratuitous recursion and regexp.

--- blah.rb
+++ blah.rb
@@ -1,88 +1,71 @@
-#class LinkedList
-#  def initialize(wordAry)
-#    first = 1
-#    puts(wordAry.size)
-#    wordAry.each do |tempNode|
-##      puts(tempNode)
-#      myTmp = ListNode.new
-#      myTmp.setName(tempNode)
-#      myTmp.printName
-#      if first == 1
-#        first = 0
-#        rootNode = myTmp
-#        prevNode = rootNode
-#      else
-#        myTmp = ListNode.new
-#        myTmp.setName(tempNode)
-#        prevNode.nextListNode(myTmp)
-#      end
-#    end
-#  end
-#end
-
-
-
 class ListNode
+  @right
+  @left
+
+
   def initialize(aWord)
     @aWord = aWord
-    @nextListNode = Array.new
   end
 
   def setNextListNode(nextListNode)
     @nextListNode.push(nextListNode)
   end
 
-#  def prevListNode(prevListNode)
-#    @prevListNode = prevListNode
-#  end
-#
-#  def getNextListNode
-#    return @nextListNode
-#  end
+
+  def setRight(aWord)
+    @right = ListNode.new(aWord)
+  end
+
+  def getRight
+    @right
+  end
+
+  def setLeft(aWord)
+    @left = ListNode.new(aWord)
+  end
+
+  def getLeft
+    @left
+  end
 
   def setName(objName)
     @objName = objName
   end
 
-  def printName()
+  def printName
     puts(@objName)
   end
-end
 
-
-def branchOut(curNode,myWords)
-  first = myWords.pop
-  if first != nil
-    a=ListNode.new(first)
-    curNode.setNextListNode(a)
-    branchOut(a,myWords)
-  end
-
-  second = myWords.pop
-  if second != nil
-    b=ListNode.new(second)
-    curNode.setNextListNode(b)
-    branchOut(b,myWords)
+  def printWord
+    puts "#{@aWord}"
   end
 end
 
-
+def branchOut(curNode,myWordsAry)
+  aWord = myWordsAry.pop
+  if aWord != nil
+    if /^[A-Ma-m]/ =~ aWord
+      puts "RIGHT: #{aWord}"
+      curNode.setRight(aWord)
+      branchOut(curNode.getRight,myWordsAry)
+    elsif /^[N-Zn-z]/ =~ aWord
+      puts "LEFT: #{aWord}"
+      curNode.setLeft(aWord)
+      branchOut(curNode.getLeft,myWordsAry)
+    else
+      puts "ERROR"
+      exit
+    end
+  end
+end
 
 
 ###############################################################
 
 someText = String.new(str="The quick brown fox jumped over the lazy dog.")
 puts(someText);
-#puts(someText.split[3])
-#myWords = someText.split;
-#puts(myWords[4])
-#puts myWords.size
-#myShuffle = myWords.shuffle
-#puts(myShuffle[4])
-#myWords = myShuffle.dup
-##rootNode = ListNode.new(myWords.pop)
-
 
 txtAry = someText.split
 rootNode = ListNode.new(txtAry.pop)
 branchOut(rootNode,txtAry)
+puts "ENDING WITH #{txtAry.size}"
